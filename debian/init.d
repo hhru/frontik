1bbf30498abfdbd788a318ceac93f0823524d3de
Andrey Tatarinov <elephantum@yandex.ru>
2010-01-13 11:52:20 +0000

    LSB info in init.d script

--- debian/init.d
+++ debian/init.d
@@ -1,6 +1,16 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+### BEGIN INIT INFO
+# Provides:          frontik
+# Required-Start:    $network $remote_fs
+# Required-Stop:     $network $remote_fs
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: Start daemon at boot time
+# Description:       Enable service provided by daemon.
+### END INIT INFO
+
 from tornado_util.supervisor import supervisor
 
 supervisor(
