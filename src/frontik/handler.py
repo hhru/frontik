a698bbf8d47196dcf68de3113058aab8e1451d56
Andrey Tatarinov <elephantum@yandex.ru>
2010-05-25 13:20:40 +0400

    fix memory leak with self.finish_group for real

--- src/frontik/handler.py
+++ src/frontik/handler.py
@@ -11,7 +11,6 @@ import httplib
 import os.path
 import time
 import urllib
-import weakref
 
 import tornado.autoreload
 import tornado.web
@@ -234,9 +233,6 @@ class PageHandlerGlobals(object):
 
 working_handlers_count = 0
 
-def weakrefd_callback(cb):
-    return lambda *args, **kw: cb()(*args, **kw) if cb() else None
-
 class PageHandler(tornado.web.RequestHandler):
     '''
     Хендлер для конкретного запроса. Создается на каждый запрос.
@@ -258,8 +254,8 @@ class PageHandler(tornado.web.RequestHandler):
 
         self.text = None
 
-        self.finish_group = frontik.async.AsyncGroup(weakrefd_callback(weakref.ref(self._finish_page)),
-                                                     log=weakrefd_callback(weakref.ref(self.log.debug)))
+        self.finish_group = frontik.async.AsyncGroup(self._finish_page,
+                                                     log=self.log.debug)
 
         self.should_dec_whc = False
 
