23bed3ac621d0546a05d481b1c9896fa88084122
p.trukhanov <p.trukhanov@hh.ru>
2011-02-17 17:27:28 +0300

    HH-16103 fixed

--- frontik/auth.py
+++ frontik/auth.py
@@ -6,9 +6,6 @@ def passed_basic_auth(handler, login, passwd):
     if auth_header:
         method, auth_b64 = auth_header.split(' ')
         given_login, given_passwd = auth_b64.decode('base64').split(':')
-
-        if login == given_login or passwd == given_passwd:
-            return True
-
+        return login == given_login and passwd == given_passwd
     return False
 
