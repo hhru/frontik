670b06886b2c82ed5b21f6aa2322597859976c87
v.katraev <v.katraev@hh.ru>
2011-11-14 17:25:26 +0300

    HH-22355 405 with allow headers for put, options, delete added

--- frontik/handler.py
+++ frontik/handler.py
@@ -248,6 +248,16 @@ class PageHandler(tornado.web.RequestHandler):
             self.get_page()
             self.finish_page()
 
+    def delete(self, *args, **kwargs):
+        raise HTTPError(405, headers={"Allow": "GET, POST"})
+
+    def put(self, *args, **kwargs):
+        raise HTTPError(405, headers={"Allow": "GET, POST"})
+
+    def options(self, *args, **kwargs):
+        raise HTTPError(405, headers={"Allow": "GET, POST"})
+
+
     def finish(self, chunk = None):
         if hasattr(self, 'whc_limit'):
             self.whc_limit.release()
