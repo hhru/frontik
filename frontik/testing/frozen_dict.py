e91ccb97114156b90286cbe456a94f6ec440bdb4
Alexander A Shaposhnikov <hamilyon@yandex.ru>
2012-03-02 13:16:02 +0300

    HH-25219 frontik apps testing infrastructure

---
+++ frontik/testing/frozen_dict.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+import collections
+
+class FrozenDict(collections.Mapping):
+    """Frozen dict for using nested structures as keys in dicts.
+    Attribute goes to Mike Graham http://stackoverflow.com/questions/2703599/what-would-be-a-frozen-dict"""
+
+    def __init__(self, *args, **kwargs):
+        self._d = dict(*args, **kwargs)
+        self._hash = None
+
+    def __iter__(self):
+        return iter(self._d)
+
+    def __len__(self):
+        return len(self._d)
+
+    def __getitem__(self, key):
+        return self._d[key]
+
+    def __hash__(self):
+        if self._hash is None:
+            self._hash = 0
+            for pair in self.iteritems():
+                self._hash ^= hash(pair)
+        return self._hash
