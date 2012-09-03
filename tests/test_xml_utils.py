c96ecd2fa35a15929e4f655eeaf124997cb142dc
AndreySumin <an.sumin@hh.ru>
2012-09-03 14:36:51 +0400

    HH-24363 add test for dict_to_xml and xml_to_dict

---
+++ tests/test_xml_utils.py
@@ -0,0 +1,55 @@
+# coding=utf-8
+
+import lxml.etree as etree
+from frontik.xml_util import xml_to_dict, dict_to_xml
+
+xml = '''
+    <root>
+        <key1>value</key1>
+        <key2></key2>
+        <nested>
+            <key>русский текст</key>
+        </nested>
+        <complexNested>
+            <nested>
+                <key>value</key>
+                <otherKey>otherValue</otherKey>
+            </nested>
+            <other>123</other>
+        </complexNested>
+    </root>
+    '''
+
+dictionary_before = {
+    'key1': 'value',
+    'key2': '',
+    'nested': {
+        'key': 'русский текст'
+    },
+    'complexNested': {
+        'nested': {
+            'key': 'value',
+            'otherKey': 'otherValue'
+        },
+        'other': 123
+    }
+}
+
+dictionary_after = {
+    'key1': 'value',
+    'key2': '',
+    'nested': {
+        'key': '&#1088;&#1091;&#1089;&#1089;&#1082;&#1080;&#1081; &#1090;&#1077;&#1082;&#1089;&#1090;'
+    },
+    'complexNested': {
+        'nested': {
+            'key': 'value',
+            'otherKey': 'otherValue'
+        },
+        'other': '123'
+    }
+}
+
+def test_xml_to_dict_and_back_again():
+    assert (xml_to_dict(etree.XML(xml)) == dictionary_after)
+    assert (xml_to_dict(dict_to_xml(dictionary_before, 'root')) == dictionary_after)
