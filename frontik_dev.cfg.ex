e42e444ce2137da427cec3d04dc77b319319d210
Andrew Sumin <sumin.andrew@gmail.com>
2013-07-25 13:58:00 +0400

    HH-35564 update configs and remove some trash

--- frontik_dev.cfg.ex
+++ frontik_dev.cfg.ex
@@ -7,14 +7,13 @@ workers_count = 1
 daemonize = False
 
 loglevel = 'debug'
-logfile = None # log to stderr
+logfile = None  # log to stderr
 
 pidfile = None
 
 urls = [
-#format:    (regexp,                    dispatcher),
-            (r".*",                     App("app_name", "/path/to/app/www_root") ),
-            (r"/echo/(?P<id>(\d+))?",   SomeHandler),
+    #format:    (regexp, dispatcher),
+    ("/page", App("hh-xhh", "/usr/share/pyshared/xhh"))
 ]
 # where dispatcher is an App or any other dispatcher, like frontik.app.RegexDispatcher for example
 
@@ -23,3 +22,13 @@ use_standart_doc = False
 debug = True
 debug_login = "hh"
 debug_password = "12345"
+
+# wait this number of seconds (int) in workers before stopping IOLoop in each worker,
+# should be a little less than supervisor_sigterm_timeout
+stop_timeout = 4
+
+# wait this number of seconds (int) in supervisor after sending SIGTERM to all workers and wait
+# 0.1*workers_count seconds more after sending SIGKILL
+supervisor_sigterm_timeout = 5
+
+log_blocked_ioloop_timeout = 2.0
