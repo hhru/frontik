6cbf8178f40453a5bcf782f49f4e7fa12f8a36e5
Andrew Sumin <sumin.andrew@gmail.com>
2013-08-05 18:00:34 +0400

    HH-37025 refactor postprocessing

--- frontik/options.py
+++ frontik/options.py
@@ -21,6 +21,7 @@ tornado.options.define('timeout_multiplier', 1.0, float)
 tornado.options.define('long_request_timeout', None, float)
 tornado.options.define('kill_long_requests', False, bool)
 
+tornado.options.define('executor_type', 'threaded', str)  # available options: 'threaded', 'ioloop'
 tornado.options.define('executor_pool_size', 1, int)
 
 tornado.options.define('graylog', False, bool)
---
+++ frontik/xsl_producer.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+
+import copy
+import weakref
+import time
+
+from lxml import etree
+import tornado.options
+
+import frontik
+import frontik.handler
+import frontik.jobs
+
+
+class XslProducer(object):
+    def __init__(self, handler):
+        self.handler = weakref.proxy(handler)
+        self.log = weakref.proxy(self.handler.log)
+
+        if tornado.options.options.executor_type == 'threaded':
+            self.executor = frontik.jobs.executor()
+        elif tornado.options.options.executor_type == 'ioloop':
+            self.executor = frontik.jobs.IOLoopExecutor
+        else:
+            raise ValueError('Cannot initialize XslProducer with executor_type {0!r}'.format(
+                tornado.options.options.executor_type))
+
+        self.xml_cache = self.handler.ph_globals.xml.xml_cache
+        self.xsl_cache = self.handler.ph_globals.xml.xsl_cache
+
+        self.doc = frontik.doc.Doc(root_node=etree.Element('doc', frontik='true'))
+        self.transform = None
+        self.transform_filename = None
+
+        if not self.handler.config.apply_xsl:
+            self.log.debug('ignoring set_xsl() because config.apply_xsl=%s', self.handler.config.apply_xsl)
+            self.apply_xsl = False
+        elif frontik.util.get_cookie_or_url_param_value(self.handler, 'noxsl') is not None:
+            self.handler.require_debug_access()
+            self.log.debug('ignoring set_xsl() because noxsl parameter is set')
+            self.apply_xsl = False
+        else:
+            self.apply_xsl = True
+
+    def __call__(self, callback):
+        if self.apply_xsl and self.transform:
+            return self.__prepare_finish_with_xsl(callback)
+        else:
+            return self.__prepare_finish_wo_xsl(callback)
+
+    def set_xsl(self, filename):
+        try:
+            self.transform_filename = filename
+            self.transform = self.xsl_cache.load(filename, log=self.log)
+        except etree.XMLSyntaxError:
+            self.log.exception('failed parsing XSL file {0} (XML syntax)'.format(filename))
+            raise frontik.handler.HTTPError(500)
+        except etree.XSLTParseError:
+            self.log.exception('failed parsing XSL file {0} (XSL parse error)'.format(filename))
+            raise frontik.handler.HTTPError(500)
+        except:
+            self.log.exception('XSL transformation error with file {0}'.format(filename))
+            raise frontik.handler.HTTPError(500)
+
+    def __prepare_finish_with_xsl(self, callback):
+        self.log.debug('finishing with xsl')
+
+        if not self.handler._headers.get('Content-Type', None):
+            self.handler.set_header('Content-Type', 'text/html')
+
+        def job():
+            t = time.time()
+            return t, str(self.transform(copy.deepcopy(self.doc.to_etree_element())))
+
+        def success_cb(response):
+            t, result = response
+            self.log.stage_tag("xsl")
+            self.log.debug('applied XSL %s in %.2fms', self.transform_filename, (time.time() - t) * 1000)
+            if len(self.transform.error_log):
+                map(self.log.info, (map('xsl message: {0.message}'.format, self.transform.error_log)))
+            callback(result)
+
+        def exception_cb(exception):
+            self.log.error('failed transformation with XSL %s', self.transform_filename)
+            self.log.error('XSL error log entries:\n{0}'.format('\n'.join(map(
+                'File "{0.filename}", line {0.line}, column {0.column}\n\t{0.message}'.format,
+                self.transform.error_log))))
+            raise exception
+
+        self.executor.add_job(job, self.handler.async_callback(success_cb), self.handler.async_callback(exception_cb))
+
+    def __prepare_finish_wo_xsl(self, callback):
+        self.log.debug('finishing wo xsl')
+        self.handler.set_header('Content-Type', 'application/xml')
+        callback(self.doc.to_string())
