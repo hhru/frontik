e5033a7e9914a8213d23cbdcd0fd9aff086f4a65
Andrey Sumin <an.sumin@hh.ru>
2013-10-16 02:48:56 +0400

    HH-37783 clean config files

--- frontik_srv.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-# -*- coding: utf-8 -*-
-from frontik.server import main
-
-
-if __name__ == "__main__":
-    main()
--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
+# coding=utf-8
+
 from setuptools import setup
 
 setup(
-    name="frontik",
-    version=__import__("frontik").__version__,
-    description="Frontik is an asyncronous Tornado-based application server",
-    long_description=open("README.md").read(),
-    url="https://github.com/hhru/frontik",
-    packages=["frontik", "frontik/testing", "frontik/testing/pages"],
-    scripts=["scripts/frontik"],
+    name='frontik',
+    version=__import__('frontik').__version__,
+    description='Frontik is an asyncronous Tornado-based application server',
+    long_description=open('README.md').read(),
+    url='https://github.com/hhru/frontik',
+    packages=['frontik', 'frontik/testing', 'frontik/testing/pages'],
+    scripts=['scripts/frontik'],
     package_data={
-        "frontik": ["*.xsl"],
+        'frontik': ['*.xsl'],
     },
     install_requires=[
         'lxml >= 2.2.8, < 2.3a',
