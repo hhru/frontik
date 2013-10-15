e3d65aa36d58150e98686d341f91407fdc25d842
Andrew Sumin <sumin.andrew@gmail.com>
2013-10-16 03:12:34 +0400

    HH-37783 add new version.py

--- setup.py
+++ setup.py
@@ -1,6 +1,20 @@
 # coding=utf-8
 
+import os
+
 from setuptools import setup
+from setuptools.command.install import install
+
+from frontik.version import parse_version_from_changelog
+
+
+class InstallHook(install):
+    def run(self):
+        frontik_build_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), self.build_lib, 'frontik')
+        with open(os.path.join(frontik_build_dir, 'version.py'), 'w') as version_file:
+            version_file.write('version = "{0}"\n'.format(parse_version_from_changelog()))
+
+        install.run(self)
 
 setup(
     name='frontik',
@@ -8,6 +22,7 @@ setup(
     description='Frontik is an asyncronous Tornado-based application server',
     long_description=open('README.md').read(),
     url='https://github.com/hhru/frontik',
+    cmdclass={'install': InstallHook},
     packages=['frontik', 'frontik/testing', 'frontik/testing/pages'],
     scripts=['scripts/frontik'],
     package_data={
