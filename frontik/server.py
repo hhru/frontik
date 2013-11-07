93a76ffbdb607a8498d2a54ed085d18625d64b3a
AndreySumin <an.sumin@hh.ru>
2013-11-07 11:26:12 +0400

    HH-38988 use tornado.server instead of tornado_util

--- frontik/server.py
+++ frontik/server.py
@@ -4,7 +4,7 @@
 import logging
 import sys
 
-import tornado_util.server
+import tornado.server
 from tornado.options import options
 
 import frontik.app
@@ -15,7 +15,7 @@ log = logging.getLogger('frontik.server')
 
 
 def main(config_file='/etc/frontik/frontik.cfg'):
-    tornado_util.server.bootstrap(config_file=config_file)
+    tornado.server.bootstrap(config_file=config_file)
     frontik_logging.bootstrap_all_logging()
 
     try:
@@ -24,4 +24,4 @@ def main(config_file='/etc/frontik/frontik.cfg'):
         log.exception('failed to initialize frontik application, quitting')
         sys.exit(1)
 
-    tornado_util.server.main(app)
+    tornado.server.main(app)
