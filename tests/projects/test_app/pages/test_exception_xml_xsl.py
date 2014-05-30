30e7529719f2f0c3593a6e1be7757aaf85808775
Andrey Sumin <an.sumin@hh.ru>
2014-05-30 16:23:13 +0400

    HH-43534 changes for tornado3 upgrade

--- tests/projects/test_app/pages/test_exception_xml_xsl.py
+++
@@ -1,9 +0,0 @@
-import frontik.handler
-from lxml import etree
-
-
-class Page(frontik.handler.PageHandler):
-    def get_page(self):
-        self.doc.put(etree.Element('not-ok'))
-        raise frontik.handler.HTTPError(302, xml=etree.Element('ok'), xsl='simple.xsl')
-        self.doc.put("absolutely not forty two, no way")
