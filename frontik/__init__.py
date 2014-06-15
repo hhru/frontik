97f54d01fd995554d964fabd6be24bac26fee6bc
Andrew Sumin <sumin.andrew@gmail.com>
2014-06-15 17:13:56 +0400

    HH-40165 update docs and misc

--- frontik/__init__.py
+++ frontik/__init__.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
+# coding=utf-8
 
 from frontik.version import version
 
 __version__ = VERSION = version
 import lxml.etree as etree
-from lxml.builder import E as etree_builder
 
 from frontik.doc import Doc
 from frontik.util import make_url
