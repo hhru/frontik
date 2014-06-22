b8260e5443c9f1439901bb529af01ec1d32e240b
Andrey Sumin <an.sumin@hh.ru>
2014-06-22 14:57:33 +0400

    HH-44215 remove __init__.py side effects

--- frontik/__init__.py
+++ frontik/__init__.py
@@ -3,9 +3,3 @@
 from frontik.version import version
 
 __version__ = VERSION = version
-import lxml.etree as etree
-
-from frontik.doc import Doc
-from frontik.util import make_url
-from frontik.util import list_unique
-from frontik.util import make_qs
