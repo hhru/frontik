556ba256a80c2248afb2a29eae655c11b670ad57
Andrey Sumin <an.sumin@hh.ru>
2014-07-16 11:57:28 +0400

    HH-44762 fix unicode test

--- tests/test_unicode.py
+++ tests/test_unicode.py
@@ -2,25 +2,28 @@
 
 import unittest
 
+from tornado.escape import to_unicode
+
+from frontik.util import make_url
 from .instances import frontik_test_app
 
 
 class TestUnicode(unittest.TestCase):
     def test_unicode_argument(self):
-        response = frontik_test_app.get_page('arguments?param=тест')
+        response = frontik_test_app.get_page(make_url('arguments', param=u'тест'))
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, '{"тест": "тест"}')
+        self.assertEqual(to_unicode(response.content), u'{"тест": "тест"}')
 
     def test_cp1251_argument(self):
         cp1251_arg = u'тест'.encode('cp1251')
-        response = frontik_test_app.get_page('arguments?param={}'.format(cp1251_arg))
+        response = frontik_test_app.get_page(make_url('arguments', param=cp1251_arg))
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, '{"тест": "тест"}')
+        self.assertEqual(to_unicode(response.content), u'{"тест": "тест"}')
 
     def test_argument_with_invalid_chars(self):
         arg_with_invalid_chars = u'≤'.encode('koi8_r') + 'тест'
-        response = frontik_test_app.get_page('arguments?param={}'.format(arg_with_invalid_chars))
+        response = frontik_test_app.get_page(make_url('arguments', param=arg_with_invalid_chars))
 
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.content, '{"тест": "тест"}')
+        self.assertEqual(to_unicode(response.content), u'{"тест": "тест"}')
