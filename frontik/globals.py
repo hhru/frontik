519bdc61751648e7f11d41330a574f9286339d56
Andrey Sumin <an.sumin@hh.ru>
2014-07-29 16:52:38 +0400

    HH-45231 move ApplicationGlobals back to handler

--- frontik/globals.py
+++ frontik/globals.py
@@ -1,21 +1,5 @@
 import time
 
-import frontik.producers.json_producer
-import frontik.producers.xml_producer
-
-import tornado.curl_httpclient
-
-
-class ApplicationGlobals(object):
-    """ Global settings for Frontik instance """
-    def __init__(self, app_package):
-        self.config = app_package.config
-
-        self.xml = frontik.producers.xml_producer.ApplicationXMLGlobals(app_package.config)
-        self.json = frontik.producers.json_producer.ApplicationJsonGlobals(app_package.config)
-
-        self.http_client = tornado.curl_httpclient.CurlAsyncHTTPClient(max_clients=200)
-
 
 class Stats(object):
     def __init__(self):
