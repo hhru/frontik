c125ee6f9a528e5f696087a2b465e690bca968d6
Andrey Sumin <an.sumin@hh.ru>
2014-12-11 12:44:15 +0300

    HH-39242 return old code for backwards compatibility

--- frontik/server.py
+++ frontik/server.py
@@ -22,8 +22,16 @@ def main(config_file=None):
             log.exception('no frontik application present (`app` option is not specified)')
             sys.exit(1)
 
+        if options.app.startswith('/') or options.app.startswith('.'):
+            import os
+            app_name = os.path.basename(os.path.normpath(options.app))
+            app = frontik.app.App(app_name, options.app)
+            app.initialize_app()
+        else:
+            app = frontik.app.NewApp(options.app)
+
         tornado_app = frontik.app.get_tornado_app(
-            options.app_root_url, frontik.app.App(options.app), options.tornado_settings
+            options.app_root_url, app, options.tornado_settings
         )
     except:
         log.exception('failed to initialize frontik application, quitting')
