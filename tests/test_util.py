b0bfbf43459fa38c06c60fd544de894b3af3ff27
Nikita Kovaliov <nikita@maizy.ru>
2015-03-26 16:17:14 +0300

    HH-51161 fix make_qs with unicode param keys

--- tests/test_util.py
+++ tests/test_util.py
@@ -1,5 +1,5 @@
 # coding=utf-8
-
+from collections import OrderedDict
 import unittest
 
 from frontik.util import make_qs
@@ -24,7 +24,20 @@ class TestUtil(unittest.TestCase):
 
     def test_make_qs_encode(self):
         query_args = {'a': u'тест', 'b': 'тест'}
-        self.assertQueriesEqual(make_qs(query_args), 'a=%D1%82%D0%B5%D1%81%D1%82&b=%D1%82%D0%B5%D1%81%D1%82')
+        qs = make_qs(query_args)
+        self.assertIsInstance(qs, str)
+        self.assertQueriesEqual(qs, 'a=%D1%82%D0%B5%D1%81%D1%82&b=%D1%82%D0%B5%D1%81%D1%82')
+
+    def test_from_ordered_dict(self):
+        qs = make_qs(OrderedDict([('z', 'я'), ('г', 'd'), ('b', ['2', '1'])]))
+        self.assertIsInstance(qs, str)
+        self.assertEqual(qs, 'z=%D1%8F&%D0%B3=d&b=2&b=1')
+
+    def test_unicode_params(self):
+        self.assertQueriesEqual(
+            make_qs({'при': 'вет', u'по': u'ка'}),
+            '%D0%BF%D1%80%D0%B8=%D0%B2%D0%B5%D1%82&%D0%BF%D0%BE=%D0%BA%D0%B0'
+        )
 
     def assertQueriesEqual(self, qs1, qs2):
         qs1_list = sorted(qs1.split('&'))
