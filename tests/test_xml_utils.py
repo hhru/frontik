56e4df73713fbd2617de05cdc4e828d3cf7cac05
Andrew Sumin <sumin.andrew@gmail.com>
2015-12-24 15:17:13 +0300

    HH-61473 add more xml_util tests

--- tests/test_xml_utils.py
+++ tests/test_xml_utils.py
@@ -1,11 +1,12 @@
 # coding=utf-8
 
+import os.path
 import unittest
 
 from lxml import etree
 from lxml_asserts.testcase import LxmlTestCaseMixin
 
-from frontik.xml_util import xml_to_dict, dict_to_xml
+from frontik.xml_util import dict_to_xml, xml_from_file, xml_to_dict
 
 XML = etree.XML('''
     <root>
@@ -68,3 +69,35 @@ class TestXmlUtils(unittest.TestCase, LxmlTestCaseMixin):
 
         self.assertEqual(xml_to_dict(dict_to_xml(DICT_BEFORE, 'root')), DICT_AFTER)
         self.assertXmlEqual(dict_to_xml(xml_to_dict(XML), 'root'), XML)
+
+    XML_FILE = os.path.join(os.path.dirname(__file__), 'projects', 'test_app', 'xml', 'aaa.xml')
+    XML_MISSING_FILE = os.path.join(os.path.dirname(__file__), 'bbb.xml')
+    XML_SYNTAX_ERROR_FILE = os.path.join(os.path.dirname(__file__), 'projects', 'test_app', 'xsl', 'syntax_error.xsl')
+
+    class MockLog(object):
+        def __init__(self):
+            self.message = None
+
+        def error(self, message, *args):
+            self.message = message % args
+
+    def test_xml_from_file(self):
+        result = xml_from_file(self.XML_FILE, TestXmlUtils.MockLog())
+        self.assertIn('Source:', result[0].text)
+        self.assertEqual(result[1].text, 'aaa')
+
+    def test_xml_from_file_does_not_exist(self):
+        log = TestXmlUtils.MockLog()
+
+        with self.assertRaises(IOError):
+            xml_from_file(self.XML_MISSING_FILE, log)
+
+        self.assertIn('failed to read xml file', log.message)
+
+    def test_xml_from_file_syntax_error(self):
+        log = TestXmlUtils.MockLog()
+
+        with self.assertRaises(etree.XMLSyntaxError):
+            xml_from_file(self.XML_SYNTAX_ERROR_FILE, log)
+
+        self.assertIn('failed to parse xml file', log.message)
