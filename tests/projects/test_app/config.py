fe539a445db02aec37a32bd0406bf8e3e1275428
Andrey Sumin <an.sumin@hh.ru>
2016-01-14 14:04:54 +0300

    HH-57276 remove unused postprocessor option

--- tests/projects/test_app/config.py
+++ tests/projects/test_app/config.py
@@ -4,20 +4,12 @@ import os
 
 from lxml import etree
 
-
 XSL_root = os.path.normpath(os.path.join(os.path.dirname(__file__), 'xsl'))
 XML_root = os.path.normpath(os.path.join(os.path.dirname(__file__), 'xml'))
 template_root = os.path.normpath(os.path.join(os.path.dirname(__file__), 'templates'))
 
 XSL_cache_limit = 1
 XML_cache_step = 1
-
-
-def post(self, data, cb):
-    self.log.debug('posprocessor called')
-    cb(data)
-
-postprocessor = post
 
 version = [etree.Element('app-version', number='last version')]
 
