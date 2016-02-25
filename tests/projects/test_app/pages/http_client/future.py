7ebdfceedc5876ab522cfacce2437c619b491cee
Andrey Sumin <an.sumin@hh.ru>
2016-02-25 12:08:51 +0300

    HH-58572 fix add_done_callback
    Conflicts:
    	frontik/async.py

--- tests/projects/test_app/pages/http_client/future.py
+++ tests/projects/test_app/pages/http_client/future.py
@@ -6,7 +6,7 @@ import frontik.handler
 class Page(frontik.handler.PageHandler):
     def get_page(self):
         state = {
-            'second_callback_must_be_synchronous': False,
+            'second_callback_must_be_async': True,
         }
 
         def main_callback(json, response):
@@ -15,7 +15,7 @@ class Page(frontik.handler.PageHandler):
             })
 
         def second_additional_callback(future):
-            state['marker_second_callback_is_sync'] = True
+            state['second_callback_must_be_async'] = False
 
         def additional_callback(future):
             assert future is request_future
@@ -24,11 +24,11 @@ class Page(frontik.handler.PageHandler):
                 'additional_callback_called': True
             })
 
-            request_future.add_done_callback(second_additional_callback)
-            assert state['second_callback_must_be_synchronous']
+            self.add_future(request_future, self.finish_group.add(second_additional_callback))
+            assert state['second_callback_must_be_async']
 
         request_future = self.post_url(self.request.host + self.request.path, callback=main_callback)
-        request_future.add_done_callback(additional_callback)
+        self.add_future(request_future, self.finish_group.add(additional_callback))
 
     def post_page(self):
         self.json.put({
