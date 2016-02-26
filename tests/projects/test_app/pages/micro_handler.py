b62954667c698f96760467849da9ad12eac7838d
Andrew Sumin <sumin.andrew@gmail.com>
2016-02-26 13:53:31 +0300

    test microhandler default page_failed

--- tests/projects/test_app/pages/micro_handler.py
+++ tests/projects/test_app/pages/micro_handler.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# coding=utf-8
 
 from frontik.handler import HTTPError
 from frontik.micro_handler import MicroHandler
@@ -16,23 +16,18 @@ class Page(MicroHandler):
     @MicroHandler.add_preprocessor(get_page)
     @MicroHandler.add_preprocessor(get_page)
     def get_page(self):
-        if self.get_argument('no-recursion', 'false') == 'true':
-            return self.json.put({
-                'GET': 'get'
-            })
-
         fail_on_error = self.get_argument('fail_on_error', 'false') == 'true'
 
         return {
             'post': self.POST(self.request.host, self.request.path, data={'param': 'post'}),
             'unicode': self.GET(u'http://фронтик.рф', u'проверка'),
-            'put': self.PUT(self.request.host, self.request.path, fail_on_error=fail_on_error),
+            'put': self.PUT(self.request.host, self.request.path + '?code=401', fail_on_error=fail_on_error),
             'delete': self.DELETE(self.request.host, self.request.path, data={'invalid_dict_value': 'true'})
         }
 
-    def get_page_requests_failed(self, name, data, response):
-        if self.get_argument('default', 'false') != 'true':
-            raise HTTPError(403, json={'fail_on_error': True})
+    @staticmethod
+    def get_page_requests_failed(name, data, response):
+        raise HTTPError(403, json={'fail_on_error': True})
 
     def get_page_requests_done(self, result):
         assert result['post'].response.code == 200
@@ -42,20 +37,24 @@ class Page(MicroHandler):
         self.json.put(result)
 
     def post_page(self):
+        if self.get_argument('fail_on_error_default', 'false') == 'true':
+            return {
+                'e': self.PUT(
+                    self.request.host, '{}?code={}'.format(self.request.path, self.get_argument('code')),
+                    fail_on_error=True
+                )
+            }
+
         self.json.put({
             'POST': self.get_argument('param')
         })
-
-        return {
-            'get': self.GET(self.request.host, self.request.uri, data={'no-recursion': 'true'})
-        }
 
     def post_page_requests_done(self, result):
         self.json.put(result)
 
     def put_page(self):
         # Testing parse_on_error=True
-        raise HTTPError(401, json={'error': 'forbidden'})
+        raise HTTPError(int(self.get_argument('code')), json={'error': 'forbidden'})
 
     def delete_page(self):
         if self.get_argument('invalid_dict_value', 'false') == 'true':
