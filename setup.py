54d0b97c5663b9067d58a22fa79ffe3d95e9db43
Andrey Sumin <sumin.andrew@gmail.com>
2016-06-24 17:35:20 +0300

    HH-60593 use pycodestyle

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ setup(
     test_suite='tests',
     tests_require=[
         'nose',
-        'pep8',
+        'pycodestyle == 2.0.0',
         'requests >= 0.8.2',
         'lxml-asserts',
     ],
