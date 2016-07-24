47c831a1c3399e4ae06abeb276263149af370c08
Andrey Sumin <sumin.andrew@gmail.com>
2016-07-24 21:49:50 +0300

    HH-61546 use concurrent.futures

--- tests/test_jobs.py
+++ tests/test_jobs.py
@@ -10,5 +10,5 @@ class TestJobs(unittest.TestCase):
         response = frontik_test_app.get_page('job_fail')
         self.assertEqual(response.status_code, 400)
 
-        html = frontik_test_app.get_page_text('job_fail?nofail=True')
-        self.assertIn('ok', html)
+        xml = frontik_test_app.get_page_text('job_fail?nofail=True')
+        self.assertIn('<ok result="True"/>', xml)
