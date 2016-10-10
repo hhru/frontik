24786f06af71ace9e681361c891f0301f881cb77
Andrey Sumin <an.sumin@hh.ru>
2016-10-10 18:01:56 +0300

    temp

--- tests/projects/keep_alive_app/__init__.py
+++
@@ -1,8 +0,0 @@
-# coding=utf-8
-
-from frontik.app import FrontikApplication
-
-
-class TestApplication(FrontikApplication):
-    def __init__(self, **settings):
-        super(TestApplication, self).__init__(**settings)
