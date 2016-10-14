e9b62cd2ad2b5537d0e72a9dfc26de54e16dc5df
Nikita Kovaliov <nikita@maizy.ru>
2016-10-14 18:18:32 +0300

    HH-63448 add future_fold and its two aliases - future_map, future_map_exception

--- frontik/async.py
+++ frontik/async.py
@@ -4,6 +4,7 @@ import time
 import logging
 
 from tornado.ioloop import IOLoop
+from tornado.concurrent import Future
 
 default_logger = logging.getLogger('frontik.async')
 
@@ -95,3 +96,45 @@ class AsyncGroup(object):
             self.try_finish()
 
         return new_cb
+
+
+def future_fold(future, result_mapper=None, exception_mapper=None):
+    """
+    Creates a new future with result or exception processed by result_mapper and exception_mapper.
+
+    If result_mapper or exception_mapper raises an exception, it will be set as an exception for the resulting future.
+    Any of the mappers can be None — then the result or exception is left as is.
+    """
+
+    res_future = Future()
+
+    def _process(func, value):
+        try:
+            processed = func(value) if func is not None else value
+        except Exception as e:
+            res_future.set_exception(e)
+            return
+        res_future.set_result(processed)
+
+    def _on_ready(wrapped_future):
+        exception = wrapped_future.exception()
+        if exception is not None:
+            if not callable(exception_mapper):
+                def default_exception_func(error):
+                    raise error
+                _process(default_exception_func, exception)
+            else:
+                _process(exception_mapper, exception)
+        else:
+            _process(result_mapper, future.result())
+
+    IOLoop.current().add_future(future, callback=_on_ready)
+    return res_future
+
+
+def future_map(future, func):
+    return future_fold(future, result_mapper=func)
+
+
+def future_map_exception(future, func):
+    return future_fold(future, exception_mapper=func)
