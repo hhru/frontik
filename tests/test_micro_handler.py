86780faf44a921b807235f8d69e76568d262bfe8
Andrey Sumin <an.sumin@hh.ru>
2017-01-25 20:21:00 +0300

    HH-66005 add simple futures support for fail_on_error

--- tests/test_micro_handler.py
+++ tests/test_micro_handler.py
@@ -9,7 +9,7 @@ from .instances import frontik_test_app
 
 class TestMicroHandler(unittest.TestCase):
     def test_simple(self):
-        json = frontik_test_app.get_page_json('micro_handler?future_execution_time=0.5')
+        json = frontik_test_app.get_page_json('micro_handler')
 
         self.assertEqual(json['put']['error'], 'forbidden')
         self.assertEqual(json['post'], {'POST': 'post'})
@@ -20,10 +20,14 @@ class TestMicroHandler(unittest.TestCase):
         self.assertEqual(future_result, 'future_result')
 
     def test_fail_on_error(self):
-        response = frontik_test_app.get_page('micro_handler?fail_on_error=true')
+        response = frontik_test_app.get_page('micro_handler?fail_on_error_request=true')
 
         self.assertEqual(response.status_code, 403)
         self.assertEqual(response.content, b'{"fail_on_error": true}')
+
+    def test_future_fail(self):
+        response = frontik_test_app.get_page('micro_handler?fail_future=true')
+        self.assertEqual(response.status_code, 500)
 
     def test_fail_on_error_default(self):
         response = frontik_test_app.get_page('micro_handler?fail_on_error_default=true&code=400', method=requests.post)
