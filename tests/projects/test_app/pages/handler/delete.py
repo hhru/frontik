7a3ba8d31a7658aafa6c47bf695557437490b28f
Andrew Sumin <sumin.andrew@gmail.com>
2017-02-06 18:48:25 +0300

    HH-66784 remove frontik.testing.service_mock

--- tests/projects/test_app/pages/handler/delete.py
+++ tests/projects/test_app/pages/handler/delete.py
@@ -1,10 +1,12 @@
+# coding=utf-8
+
 import frontik.handler
 
 
 class Page(frontik.handler.PageHandler):
     def get_page(self):
         self.json.put(
-            self.delete_url(self.request.host + self.request.path, data={'data': 'true'})
+            self.delete_url('http://' + self.request.host + self.request.path, data={'data': 'true'})
         )
 
     def delete_page(self):
