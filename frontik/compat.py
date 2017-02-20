f1046bcca028f06486843cfffd6861d1203c117a
Andrey Sumin <an.sumin@hh.ru>
2017-02-20 15:51:49 +0300

    HH-66949 use tornado_mock.httpclient

--- frontik/compat.py
+++ frontik/compat.py
@@ -4,14 +4,13 @@ import sys
 
 __all__ = [
     'basestring_type', 'iteritems', 'long_type', 'SimpleCookie', 'unicode_type',
-    'unquote_plus', 'urlencode', 'urlparse'
+    'urlencode', 'urlparse'
 ]
 
 PY3 = sys.version_info >= (3,)
 
 if PY3:
     import urllib.parse as urlparse
-    from urllib.parse import unquote_plus
     from urllib.parse import urlencode
 
     basestring_type = str
@@ -22,7 +21,6 @@ if PY3:
         return d.items(**kw)
 
 else:
-    from urllib import unquote_plus
     from urllib import urlencode
     import urlparse
 
