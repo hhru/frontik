fe5a878691d221896125ef58543d35e5822d5518
Andrey Sumin <an.sumin@hh.ru>
2017-02-28 13:42:07 +0300

    HH-67114 introduce RequestContext in test mode

---
+++ frontik/request_context.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+
+import threading
+
+
+class RequestContext(object):
+    _state = threading.local()
+    _state.data = {}
+
+    def __init__(self, data):
+        self._data = data
+
+    @classmethod
+    def get(cls, param):
+        if not hasattr(cls._state, 'data'):
+            cls._state.data = {}
+
+        return cls._state.data.get(param)
+
+    @classmethod
+    def set(cls, param, value):
+        if not hasattr(cls._state, 'data'):
+            cls._state.data = {}
+
+        cls._state.data[param] = value
+
+    def __enter__(self):
+        self._prev_data = getattr(self.__class__._state, 'data', {})
+        self.__class__._state.data = self._data
+
+    def __exit__(self, *exc):
+        self.__class__._state.data = self._prev_data
+        del self._prev_data
+        return False
