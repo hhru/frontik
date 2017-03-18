2575a63228d15067aa2f06a22dbca30ee3d0187e
Andrew Sumin <sumin.andrew@gmail.com>
2017-03-18 16:33:43 +0300

    HH-67889 cleanup routing

--- tests/projects/re_app/config.py
+++ tests/projects/re_app/config.py
@@ -2,7 +2,7 @@
 
 import os
 
-from frontik.app import FileMappingDispatcher
+from frontik.routing import FileMappingRouter
 
 from . import pages
 from .pages import exception_on_prepare
@@ -17,9 +17,9 @@ XSL_cache_limit = 1
 template_root = os.path.normpath(os.path.join(os.path.dirname(__file__), 'jinja'))
 
 urls = [
-    ('/id/(?P<id>[^/]+)', pages.id_param.Page),
-    ('/id/(?P<id1>[^/]+)/(?P<id2>[^/]+)', pages.handler_404.Page, 'two_ids'),
-    ('/not_simple', pages.simple.Page),
-    ('/exception_on_prepare_regex', pages.exception_on_prepare.Page),
-    ('(?!/not_matching_regex)', FileMappingDispatcher(pages, handler_404=pages.handler_404.Page))
+    ('/id/(?P<id>[^/]+)', id_param.Page),
+    ('/id/(?P<id1>[^/]+)/(?P<id2>[^/]+)', handler_404.Page, 'two_ids'),
+    ('/not_simple', simple.Page),
+    ('/exception_on_prepare_regex', exception_on_prepare.Page),
+    ('(?!/not_matching_regex)', FileMappingRouter(pages))
 ]
