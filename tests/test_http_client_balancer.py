1cd9ecfa269541c4d5a1abc7662fb4f7ec4eebf8
Smirnov Danil <d.smirnov@hh.ru>
2017-10-05 20:21:22 +0300

    HH-72947 Fix slow start

--- tests/test_http_client_balancer.py
+++ tests/test_http_client_balancer.py
@@ -18,8 +18,8 @@ def _total_requests(upstream):
 
 class TestHttpClientBalancer(unittest.TestCase):
     @staticmethod
-    def _upstream(servers):
-        return Upstream('upstream', {}, servers)
+    def _upstream(servers, config=None):
+        return Upstream('upstream', {} if config is None else config, servers)
 
     def test_create(self):
         upstream = self._upstream([Server('1', 1), Server('2', 1)])
@@ -58,6 +58,18 @@ class TestHttpClientBalancer(unittest.TestCase):
 
         self.assertEqual(len(upstream.servers), 2)
         self.assertEqual(_total_weight(upstream), 7)
+
+    def test_remove_add_server_one_by_one(self):
+        upstream = self._upstream([Server('1', 1), Server('2', 1)])
+        upstream.update({}, [Server('2', 1)])
+
+        self.assertEqual(len(upstream.servers), 2)
+        self.assertEqual(_total_weight(upstream), 1)
+
+        upstream.update({}, [Server('2', 1), Server('3', 10)])
+
+        self.assertEqual(len(upstream.servers), 2)
+        self.assertEqual(_total_weight(upstream), 11)
 
     def test_borrow_server(self):
         upstream = self._upstream([Server('1', 2), Server('2', 1)])
@@ -113,3 +125,40 @@ class TestHttpClientBalancer(unittest.TestCase):
 
         _, address = upstream.borrow_server()
         self.assertEqual(address, '3')
+
+    def test_slow_start(self):
+        max_fails = 3
+        disabled_server = Server('1', 2)
+        disabled_server.disable()
+        upstream = self._upstream([disabled_server, Server('2', 1)], {'max_fails': max_fails})
+
+        fd, address = upstream.borrow_server()
+        self.assertEqual(address, '2')
+        upstream.return_server(fd)
+
+        upstream._restore_server(disabled_server)
+
+        # during slow start
+        for i in range(max_fails):
+            disabled_server_fd, address = upstream.borrow_server()
+            self.assertEqual(address, '1')
+
+            fd, address = upstream.borrow_server()
+            self.assertEqual(address, '2')
+            upstream.return_server(fd)
+
+            fd, address = upstream.borrow_server()
+            self.assertEqual(address, '2')
+            upstream.return_server(fd)
+
+            upstream.return_server(disabled_server_fd)
+
+        # after slow start
+        fd, address = upstream.borrow_server()
+        self.assertEqual(address, '1')
+
+        fd, address = upstream.borrow_server()
+        self.assertEqual(address, '2')
+
+        fd, address = upstream.borrow_server()
+        self.assertEqual(address, '1')
