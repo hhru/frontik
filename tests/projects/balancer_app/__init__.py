bd48c55bdc3ef9d7f72b1d8b0ccfeba88797a20b
Smirnov Danil <d.smirnov@hh.ru>
2017-10-23 14:28:16 +0300

    HH-73662 Fix connect timeout retry, add logging

--- tests/projects/balancer_app/__init__.py
+++ tests/projects/balancer_app/__init__.py
@@ -8,5 +8,9 @@ def get_server(handler, type):
     return Server('127.0.0.1:' + handler.get_argument(type))
 
 
+def get_non_listening_server():
+    return Server('http://example.com:81')
+
+
 class TestApplication(FrontikApplication):
     pass
