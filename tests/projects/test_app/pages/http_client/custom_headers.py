034c79c524c453c56f56c75a3d4f24229c11fba5
Andrey Sumin <sumin.andrew@gmail.com>
2019-06-22 14:00:45 +0300

    HH-93811 http client can be used after handler is finished

--- tests/projects/test_app/pages/http_client/custom_headers.py
+++ tests/projects/test_app/pages/http_client/custom_headers.py
@@ -6,6 +6,7 @@ class Page(frontik.handler.PageHandler):
         self.json.put(self.post_url(self.request.host, self.request.path))
 
     def modify_http_client_request(self, balanced_request):
+        super().modify_http_client_request(balanced_request)
         balanced_request.headers['X-Foo'] = 'Bar'
 
     def post_page(self):
