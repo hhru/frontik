6b5d2e4a1f0c74cdfc1a323300eaae0130372ed7
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2019-11-11 19:13:28 +0300

    HH-99946 make callback lazy to access options

--- frontik/timeout_tracking.py
+++ frontik/timeout_tracking.py
@@ -7,35 +7,46 @@ from tornado.options import options
 
 
 timeout_tracking_logger = logging.getLogger('timeout_tracking')
-__timeout_counters = Counter()
+_timeout_counters = Counter()
 
 
-def __send_stats(interval_ms):
-    timeout_tracking_logger.debug('timeout stats size: %d', len(__timeout_counters))
-    for data, count in __timeout_counters.items():
-        timeout_tracking_logger.error('For last %d ms, got %d requests from <%s> expecting timeout=%d ms, '
-                                      'but calling upstream <%s> from handler <%s> with timeout %d ms, '
-                                      'arbitrary we spend %d ms before the call',
-                                      interval_ms,
-                                      count,
-                                      data.outer_caller,
-                                      data.outer_timeout_ms,
-                                      data.upstream,
-                                      data.handler,
-                                      data.request_timeout_ms,
-                                      data.already_spent_time_ms)
-    __timeout_counters.clear()
+class Sender:
+    @property
+    def send_stats_callback(self):
+        if not hasattr(self, '_send_stats_callback'):
+            if options.send_timeout_stats_interval_ms:
+                self._send_stats_callback = PeriodicCallback(
+                    partial(self.__send_stats, options.send_timeout_stats_interval_ms),
+                    options.send_timeout_stats_interval_ms)
+            else:
+                self._send_stats_callback = None
+        return self._send_stats_callback
+
+    @staticmethod
+    def __send_stats(interval_ms):
+        timeout_tracking_logger.debug('timeout stats size: %d', len(_timeout_counters))
+        for data, count in _timeout_counters.items():
+            timeout_tracking_logger.error('For last %d ms, got %d requests from <%s> expecting timeout=%d ms, '
+                                          'but calling upstream <%s> from handler <%s> with timeout %d ms, '
+                                          'arbitrary we spend %d ms before the call',
+                                          interval_ms,
+                                          count,
+                                          data.outer_caller,
+                                          data.outer_timeout_ms,
+                                          data.upstream,
+                                          data.handler,
+                                          data.request_timeout_ms,
+                                          data.already_spent_time_ms)
+        _timeout_counters.clear()
 
 
-if options.send_timeout_stats_interval_ms:
-    __send_stats_callback = PeriodicCallback(partial(__send_stats, options.send_timeout_stats_interval_ms),
-                                             options.send_timeout_stats_interval_ms)
+__sender = Sender()
 
 
 def get_timeout_checker(outer_caller, outer_timeout_ms, time_since_outer_request_start_ms_supplier, *,
                         threshold_ms=100):
-    if options.send_timeout_stats_interval_ms and not __send_stats_callback.is_running():
-        __send_stats_callback.start()
+    if __sender.send_stats_callback and not __sender.send_stats_callback.is_running():
+        __sender.send_stats_callback.start()
     return TimeoutChecker(outer_caller, outer_timeout_ms, time_since_outer_request_start_ms_supplier,
                           threshold_ms=threshold_ms)
 
@@ -86,4 +97,4 @@ class TimeoutChecker:
                                                   request.upstream.name if request.upstream else None,
                                                   request_timeout_ms,
                                                   already_spent_time_ms)
-                __timeout_counters[data] += 1
+                _timeout_counters[data] += 1
