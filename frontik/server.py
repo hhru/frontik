4d44f2c6a82876c7de06f6d7d003074cf5702a28
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2020-01-22 16:37:15 +0300

    HH-102086 make server start beautiful

--- frontik/server.py
+++ frontik/server.py
@@ -1,19 +1,21 @@
+import asyncio
+import errno
+import gc
 import importlib
 import logging
 import os.path
 import signal
-import errno
 import sys
 import time
-import gc
 from concurrent.futures import ThreadPoolExecutor
+from typing import Type
 
 import tornado.autoreload
 import tornado.httpserver
 import tornado.ioloop
-from tornado import gen
 from tornado.httputil import HTTPServerRequest
 from tornado.options import parse_command_line, parse_config_file
+from tornado.platform.asyncio import BaseAsyncIOLoop
 from tornado.util import errno_from_exception
 
 from frontik.app import FrontikApplication
@@ -24,108 +26,54 @@ from frontik.request_context import get_request
 log = logging.getLogger('server')
 
 
-def parse_configs(config_files):
-    """Reads command line options / config file and bootstraps logging.
-    """
+def main(config_file=None):
+    parse_configs(config_files=config_file)
 
-    parse_command_line(final=False)
+    if options.app is None:
+        log.exception('no frontik application present (`app` option is not specified)')
+        sys.exit(1)
 
-    if options.config:
-        configs_to_read = options.config
-    else:
-        configs_to_read = config_files
-
-    configs_to_read = filter(
-        None, [configs_to_read] if not isinstance(configs_to_read, (list, tuple)) else configs_to_read
-    )
-
-    for config in configs_to_read:
-        parse_config_file(config, final=False)
-
-    # override options from config with command line options
-    parse_command_line(final=False)
-
-    bootstrap_core_logging()
-
-    for config in configs_to_read:
-        log.debug('using config: %s', config)
-        if options.autoreload:
-            tornado.autoreload.watch(config)
-
-
-def run_server(app: FrontikApplication):
-    """Starts Frontik server for an application"""
+    log.info('starting application %s', options.app)
 
     try:
-        if options.asyncio_task_threshold_sec is not None:
-            slow_tasks_logger = bootstrap_logger('slow_tasks', logging.WARNING, use_json_formatter=False)
+        module = importlib.import_module(options.app)
+    except Exception as e:
+        log.exception('failed to import application module "%s": %s', options.app, e)
+        sys.exit(1)
 
-            import asyncio
-            import reprlib
+    if options.app_class is not None and not hasattr(module, options.app_class):
+        log.exception('application class "%s" not found', options.app_class)
+        sys.exit(1)
 
-            reprlib.aRepr.maxother = 256
-            old_run = asyncio.Handle._run
+    application = getattr(module, options.app_class) if options.app_class is not None else FrontikApplication
 
-            def run(self):
-                start_time = self._loop.time()
-                old_run(self)
-                delta = self._loop.time() - start_time
-                if delta >= options.asyncio_task_threshold_sec:
-                    slow_tasks_logger.warning('%s took %.2fms', self, delta * 1000)
-                if options.asyncio_task_critical_threshold_sec and delta >= options.asyncio_task_critical_threshold_sec:
-                    request = get_request() or HTTPServerRequest('GET', '/asyncio_long_task_stub')
-                    sentry_logger = app.get_sentry_logger(request)
-                    sentry_logger.update_user_info(ip='127.0.0.1')
+    try:
+        app = application(app_root=os.path.dirname(module.__file__), **options.as_dict())
 
-                    if sentry_logger:
-                        slow_tasks_logger.warning('no sentry logger available')
-                        sentry_logger.capture_message(f'{self} took {(delta * 1000):.2f} ms', stack=True)
+        gc.disable()
+        gc.collect()
+        gc.freeze()
 
-            asyncio.Handle._run = run
+        if options.workers != 1:
+            fork_processes(options.workers)
 
-        log.info('starting server on %s:%s', options.host, options.port)
-        http_server = tornado.httpserver.HTTPServer(app, xheaders=options.xheaders)
-        http_server.bind(options.port, options.host, reuse_port=options.reuse_port)
-        http_server.start()
+        gc.enable()
 
-        io_loop = tornado.ioloop.IOLoop.current()
+        ioloop = tornado.ioloop.IOLoop.current()
 
-        if options.autoreload:
-            tornado.autoreload.start(1000)
+        executor = ThreadPoolExecutor(options.common_executor_pool_size)
+        ioloop.asyncio_loop.set_default_executor(executor)
+        initialize_application_task = ioloop.asyncio_loop.create_task(_init_app(app, ioloop))
 
-        def sigterm_handler(signum, frame):
-            log.info('requested shutdown')
-            log.info('shutting down server on %s:%d', options.host, options.port)
-            io_loop.add_callback_from_signal(server_stop)
-            signal.signal(signal.SIGTERM, signal.SIG_IGN)
-
-        def ioloop_is_running():
-            return io_loop.asyncio_loop.is_running()
-
-        def server_stop():
-            deinit_futures = [integration.deinitialize_app(app) for integration in app.available_integrations]
-            if deinit_futures:
-                def await_deinit(future):
-                    if future.exception() is not None:
-                        log.error('failed to deinit, deinit returned: %s', future.exception())
-
-                io_loop.add_future(gen.multi(deinit_futures), await_deinit)
-            http_server.stop()
-
-            if ioloop_is_running():
-                log.info('going down in %s seconds', options.stop_timeout)
-
-                def ioloop_stop():
-                    if ioloop_is_running():
-                        log.info('stopping IOLoop')
-                        io_loop.stop()
-                        log.info('stopped')
-
-                io_loop.add_timeout(time.time() + options.stop_timeout, ioloop_stop)
-
-        signal.signal(signal.SIGTERM, sigterm_handler)
+        def initialize_application_task_result_handler(future):
+            if future.exception():
+                ioloop.stop()
+        initialize_application_task.add_done_callback(initialize_application_task_result_handler)
+        ioloop.start()
+        # to raise init exception if any
+        initialize_application_task.result()
     except Exception:
-        log.exception('failed to start Tornado application')
+        log.exception('frontik application exited with exception')
         sys.exit(1)
 
 
@@ -184,67 +132,115 @@ def fork_processes(num_processes):
     sys.exit(0)
 
 
-def main(config_file=None):
-    parse_configs(config_files=config_file)
+async def run_server(app: FrontikApplication, ioloop: BaseAsyncIOLoop):
+    """Starts Frontik server for an application"""
 
-    if options.app is None:
-        log.exception('no frontik application present (`app` option is not specified)')
-        sys.exit(1)
+    if options.asyncio_task_threshold_sec is not None:
+        slow_tasks_logger = bootstrap_logger('slow_tasks', logging.WARNING, use_json_formatter=False)
 
-    log.info('starting application %s', options.app)
+        import asyncio
+        import reprlib
 
-    try:
-        module = importlib.import_module(options.app)
-    except Exception as e:
-        log.exception('failed to import application module "%s": %s', options.app, e)
-        sys.exit(1)
+        reprlib.aRepr.maxother = 256
+        wrap_handle_with_time_logging(asyncio.Handle, app, slow_tasks_logger)
 
-    if options.app_class is not None and not hasattr(module, options.app_class):
-        log.exception('application class "%s" not found', options.app_class)
-        sys.exit(1)
+    log.info('starting server on %s:%s', options.host, options.port)
+    http_server = tornado.httpserver.HTTPServer(app, xheaders=options.xheaders)
+    http_server.bind(options.port, options.host, reuse_port=options.reuse_port)
+    http_server.start()
 
-    application = getattr(module, options.app_class) if options.app_class is not None else FrontikApplication
+    if options.autoreload:
+        tornado.autoreload.start(1000)
 
-    try:
-        app = application(app_root=os.path.dirname(module.__file__), **options.as_dict())
+    def sigterm_handler(signum, frame):
+        log.info('requested shutdown')
+        log.info('shutting down server on %s:%d', options.host, options.port)
+        ioloop.add_callback_from_signal(server_stop)
+        signal.signal(signal.SIGTERM, signal.SIG_IGN)
 
-        gc.disable()
-        gc.collect()
-        gc.freeze()
+    def ioloop_is_running():
+        return ioloop.asyncio_loop.is_running()
 
-        if options.workers != 1:
-            fork_processes(options.workers)
+    def server_stop():
+        ioloop.asyncio_loop.create_task(deinit_app(app, ioloop))
+        http_server.stop()
 
-        gc.enable()
+        if ioloop_is_running():
+            log.info('going down in %s seconds', options.stop_timeout)
 
-        ioloop = tornado.ioloop.IOLoop.current()
+            def ioloop_stop():
+                if ioloop_is_running():
+                    log.info('stopping IOLoop')
+                    ioloop.stop()
+                    log.info('stopped')
 
-        executor = ThreadPoolExecutor(options.common_executor_pool_size)
-        ioloop.asyncio_loop.set_default_executor(executor)
+            ioloop.add_timeout(time.time() + options.stop_timeout, ioloop_stop)
 
-        def _async_init_cb():
-            try:
-                init_futures = app.init_async()
+    signal.signal(signal.SIGTERM, sigterm_handler)
 
-                if init_futures:
-                    def await_init(future):
-                        if future.exception() is not None:
-                            log.error('failed to initialize application, init_async returned: %s', future.exception())
-                            sys.exit(1)
 
-                        run_server(app)
+def parse_configs(config_files):
+    """Reads command line options / config file and bootstraps logging.
+    """
 
-                    ioloop.add_future(gen.multi(init_futures), await_init)
-                else:
-                    run_server(app)
+    parse_command_line(final=False)
 
-            except Exception:
-                log.exception('failed to initialize application')
-                sys.exit(1)
+    if options.config:
+        configs_to_read = options.config
+    else:
+        configs_to_read = config_files
 
-        ioloop.add_callback(_async_init_cb)
-        ioloop.start()
+    configs_to_read = filter(
+        None, [configs_to_read] if not isinstance(configs_to_read, (list, tuple)) else configs_to_read
+    )
 
-    except Exception:
-        log.exception('frontik application exited with exception')
-        sys.exit(1)
+    for config in configs_to_read:
+        parse_config_file(config, final=False)
+
+    # override options from config with command line options
+    parse_command_line(final=False)
+
+    bootstrap_core_logging()
+
+    for config in configs_to_read:
+        log.debug('using config: %s', config)
+        if options.autoreload:
+            tornado.autoreload.watch(config)
+
+
+async def _init_app(app: FrontikApplication, ioloop: BaseAsyncIOLoop):
+    initialization_futures = [future for future in app.init_async() if future]
+    if not initialization_futures:
+        await run_server(app, ioloop)
+    else:
+        await ioloop.asyncio_loop.create_task(asyncio.gather(*initialization_futures, run_server(app, ioloop)))
+
+
+async def deinit_app(app: FrontikApplication, ioloop: BaseAsyncIOLoop):
+    deinit_futures = [integration.deinitialize_app(app) for integration in app.available_integrations]
+    if deinit_futures:
+        try:
+            await asyncio.gather(*[future for future in deinit_futures if future], loop=ioloop.asyncio_loop)
+        except Exception as e:
+            log.error('failed to deinit, deinit returned: %s', e)
+
+
+def wrap_handle_with_time_logging(handle: Type[asyncio.Handle], app: FrontikApplication, slow_tasks_logger):
+    old_run = handle._run
+
+    def run(self):
+        start_time = self._loop.time()
+        old_run(self)
+        delta = self._loop.time() - start_time
+        if delta >= options.asyncio_task_threshold_sec:
+            slow_tasks_logger.warning('%s took %.2fms', self, delta * 1000)
+        if options.asyncio_task_critical_threshold_sec and delta >= options.asyncio_task_critical_threshold_sec:
+            request = get_request() or HTTPServerRequest('GET', '/asyncio_long_task_stub')
+            sentry_logger = app.get_sentry_logger(request)
+            sentry_logger.update_user_info(ip='127.0.0.1')
+
+            if sentry_logger:
+                slow_tasks_logger.warning('no sentry logger available')
+                sentry_logger.capture_message(f'{self} took {(delta * 1000):.2f} ms', stack=True)
+
+    asyncio.Handle._run = run
