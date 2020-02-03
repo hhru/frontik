f96e1c14392d1fd6ed7602557fdd9dab34255e49
Dmitriy Zharikhin <d.zharikhin@hh.ru>
2020-02-03 17:53:09 +0300

    HH-103704 fix fork workers

--- frontik/process.py
+++ frontik/process.py
@@ -13,15 +13,16 @@ log = logging.getLogger('fork')
 def fork_workers(worker_function, *, num_workers, after_workers_up_action, before_workers_shutdown_action):
     log.info("starting %d processes", num_workers)
     children = {}
+    is_worker = False
     for i in range(num_workers):
-        id = _start_child(i, children)
-        if id is not None:
+        is_worker = _start_child(i, children) is not None
+        if is_worker:
             worker_function()
-            raise Exception('no way to normally return from worker function')
-    _supervise_workers(children, after_workers_up_action, before_workers_shutdown_action)
+    if not is_worker:
+        _supervise_workers(children, worker_function, after_workers_up_action, before_workers_shutdown_action)
 
 
-def _supervise_workers(children, after_workers_up_action, before_workers_shutdown_action):
+def _supervise_workers(children, worker_function, after_workers_up_action, before_workers_shutdown_action):
     gc.enable()
 
     def sigterm_handler(signum, frame):
@@ -52,9 +53,9 @@ def _supervise_workers(children, after_workers_up_action, before_workers_shutdow
             log.info("child %d (pid %d) exited normally", id, pid)
             continue
 
-        new_id = _start_child(id, children)
-        if new_id is not None:
-            return new_id
+        is_worker = _start_child(id, children) is not None
+        if is_worker:
+            worker_function()
 
     log.info('all children terminated, exiting')
     sys.exit(0)
