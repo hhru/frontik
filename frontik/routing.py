6da77ff351861f1a26656aec9c9d048443f7134d
boksh <e.bokshitskiy@hh.ru>
2020-10-04 22:48:41 +0300

    HH-114827 rename function

--- frontik/routing.py
+++ frontik/routing.py
@@ -35,7 +35,7 @@ class FileMappingRouter(Router):
             routing_logger.info('page module name exceeds PATH_MAX (%s), using 404 page', MAX_MODULE_NAME_LENGTH)
             return _get_application_404_handler_delegate(application, request)
 
-        def _handle_exception_if_module_is_found_but_import_fail():
+        def _handle_general_module_import_exception():
             routing_logger.exception('error while importing %s module', page_module_name)
             return _get_application_500_handler_delegate(application, request)
 
@@ -44,11 +44,11 @@ class FileMappingRouter(Router):
             routing_logger.debug('using %s from %s', page_module_name, page_module.__file__)
         except ModuleNotFoundError as module_not_found_error:
             if module_not_found_error.name != page_module_name:
-                return _handle_exception_if_module_is_found_but_import_fail()
+                return _handle_general_module_import_exception()
             routing_logger.warning('%s module not found', (self.name, page_module_name))
             return _get_application_404_handler_delegate(application, request)
         except Exception:
-            return _handle_exception_if_module_is_found_but_import_fail()
+            return _handle_general_module_import_exception()
 
         if not hasattr(page_module, 'Page'):
             routing_logger.error('%s.Page class not found', page_module_name)
