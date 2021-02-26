99d72a159a78e74f635a025da920630922442219
Basharin Anton <a.basharin@hh.ru>
2021-02-26 13:51:46 +0300

    HH-124497 add validation model test

--- tests/test_arguments.py
+++ tests/test_arguments.py
@@ -9,10 +9,17 @@ class TestJsonResponse(unittest.TestCase):
         response = frontik_test_app.get_page('validate_arguments?list=1&list=2&string=safestring', notpl=True)
 
         data = json.loads(response.content)
-        self.assertEqual(data['list'], [1, 2])
-        self.assertEqual(data['string'], 'safestring')
+        self.assertEqual([1, 2], data['list'])
+        self.assertEqual('safestring', data['string'])
 
     def test_validation_failed(self):
         response = frontik_test_app.get_page('validate_arguments?list=1&list=2&string=un/safe', notpl=True)
 
         self.assertEqual(response.status_code, 400)
+
+    def test_validation_model(self):
+        response = frontik_test_app.get_page('validate_arguments?list=1&list=2&string=nword&model=true', notpl=True)
+
+        data = json.loads(response.content)
+        self.assertEqual([1, 2], data['list'])
+        self.assertEqual('customString', data['string'])
