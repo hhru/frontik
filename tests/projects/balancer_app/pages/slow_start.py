c239aee138d1b5073c7c99e7cbefffd76d11ee53
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2021-04-22 17:25:24 +0300

    HH-128262 up balancing client (fix parsing slow start config)

---
+++ tests/projects/balancer_app/pages/slow_start.py
@@ -0,0 +1,47 @@
+import time
+
+from http_client import Upstream, Server
+
+from frontik import media_types
+from frontik.handler import PageHandler
+from frontik.futures import AsyncGroup
+
+from tests.projects.balancer_app import get_server
+from tests.projects.balancer_app.pages import check_all_requests_done, check_all_servers_occupied
+
+
+class Page(PageHandler):
+    def get_page(self):
+        server = get_server(self, 'normal')
+        server.weight = 5
+
+        server_slow_start = Server('127.0.0.1:12345', weight=5, dc='Test')
+
+        self.application.upstream_caches.upstreams['slow_start'] = Upstream('slow_start',
+                                                                            {'slow_start_interval_sec': '0.1'},
+                                                                            [server])
+        self.text = ''
+
+        def check_requests_cb():
+            check_all_requests_done(self, 'slow_start')
+
+        async_group = AsyncGroup(check_requests_cb)
+
+        def callback_post(text, response):
+            self.text = text
+
+        self.post_url('slow_start', self.request.path, callback=async_group.add(callback_post))
+        time.sleep(0.2)
+        self.application.upstream_caches.upstreams['slow_start'] = Upstream('slow_start',
+                                                                            {'slow_start_interval_sec': '10'},
+                                                                            [server, server_slow_start])
+
+        self.post_url('slow_start', self.request.path, callback=async_group.add(callback_post))
+        self.post_url('slow_start', self.request.path, callback=async_group.add(callback_post))
+        self.post_url('slow_start', self.request.path, callback=async_group.add(callback_post))
+
+        check_all_servers_occupied(self, 'slow_start')
+
+    def post_page(self):
+        self.add_header('Content-Type', media_types.TEXT_PLAIN)
+        self.text = str(self.application.http_client_factory.upstreams['slow_start'].servers[1].requests)
