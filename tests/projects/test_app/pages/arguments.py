1d546a4a0152667fb0c39106acd533474c3302c7
Ilya Safonov <breakjessy@gmail.com>
2024-10-17 16:20:49 +0300

    HH-234357: test_unicode.py

--- tests/projects/test_app/pages/arguments.py
+++
@@ -1,7 +0,0 @@
-from frontik.handler import PageHandler, get_current_handler
-from frontik.routing import router
-
-
-@router.get('/arguments', cls=PageHandler)
-async def get_page(handler: PageHandler = get_current_handler()) -> None:
-    handler.json.put({'тест': handler.get_argument('param')})
