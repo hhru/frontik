440dcf5a03964c23f340373db347aaa10ebfa6ad
Leonid Vinogradov <l.vinogradov@hh.ru>
2024-11-08 16:40:31 +0300

    HH-237442 increase chunk queue size

--- frontik/tornado_request.py
+++ frontik/tornado_request.py
@@ -6,6 +6,6 @@ from tornado import httputil
 class FrontikTornadoServerRequest(httputil.HTTPServerRequest):
     def __init__(self, *args, **kwargs) -> None:  # type: ignore
         super().__init__(*args, **kwargs)
-        self.body_chunks: asyncio.Queue = asyncio.Queue(maxsize=100)
+        self.body_chunks: asyncio.Queue = asyncio.Queue(maxsize=100000)
         self.request_id = None
         self.finished = False
