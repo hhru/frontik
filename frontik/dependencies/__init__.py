61b1a2dd5ab66708f42871116a41841e9cd6f1d1
Leonid Vinogradov <l.vinogradov@hh.ru>
2024-12-18 16:14:07 +0300

    HH-242556 better dependencies naming

--- frontik/dependencies/__init__.py
+++ frontik/dependencies/__init__.py
@@ -1,26 +1,26 @@
 import contextvars
 from typing import Annotated, Any
 
-import http_client
-from fastapi import Depends
+import http_client as balancing_http_client
+from fastapi import Depends, Request
 
 from frontik.app_integrations import statsd
 
 clients: contextvars.ContextVar = contextvars.ContextVar('clients')
 
 
-async def _get_app_config() -> Any:
-    return clients.get().get('app_config')
+async def get_app_config(request: Request) -> Any:
+    return request.app.config
 
 
-async def _get_http_client() -> http_client.HttpClient:
+async def get_http_client() -> balancing_http_client.HttpClient:
     return clients.get().get('http_client')
 
 
-async def _get_statsd_client() -> statsd.StatsDClient:
+async def get_statsd_client() -> statsd.StatsDClient:
     return clients.get().get('statsd_client')
 
 
-StatsDClient = Annotated[statsd.StatsDClient, Depends(_get_statsd_client)]
-AppConfig = Annotated[Any, Depends(_get_app_config)]
-HttpClient = Annotated[http_client.HttpClient, Depends(_get_http_client)]
+StatsDClient = Annotated[statsd.StatsDClient, Depends(get_statsd_client)]
+AppConfig = Annotated[Any, Depends(get_app_config)]
+HttpClient = Annotated[balancing_http_client.HttpClient, Depends(get_http_client)]
