706a8ff1285159ef49afe1ac769106a8472ebf3f
Leonid Vinogradov <l.vinogradov@hh.ru>
2024-12-18 16:14:07 +0300

    HH-242556 better dependencies naming

--- examples/example_app/pages/example.py
+++ examples/example_app/pages/example.py
@@ -1,11 +1,11 @@
 from fastapi import APIRouter
 
-from frontik.dependencies import http_client
+from frontik.dependencies import HttpClient
 
 router = APIRouter()
 
 
 @router.get('/example')
-async def example_page() -> dict:
+async def example_page(http_client: HttpClient) -> dict:
     result = await http_client.get_url('http://example.com', '/')
     return {'example': result.status_code}
