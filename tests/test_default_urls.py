eaa010963c4303fbb965ecf59bbeb2140dd1bdb6
Dmitriy Shinkarev <d.shinkarev@hh.ru>
2025-02-05 11:24:29 +0300

    HH-236539 add application version to status info

--- tests/test_default_urls.py
+++ tests/test_default_urls.py
@@ -6,17 +6,22 @@ from frontik.app import FrontikApplication
 from frontik.testing import FrontikTestBase
 
 
+class AppWithVersion(FrontikApplication):
+    def application_version(self) -> int:
+        return '0.1.0'
+
+
 class TestDefaultUrls(FrontikTestBase):
     @pytest.fixture(scope='class')
     def frontik_app(self) -> FrontikApplication:
-        return FrontikApplication(app_module_name=None)
+        return AppWithVersion(app_module_name=None)
 
-    async def test_version(self) -> None:
+    async def test_version(self, frontik_app: AppWithVersion) -> None:
         xml = await self.fetch_xml('/version')
-        test_app_version = xml.xpath('application[@name="frontik.app"]/version')[0]
+        test_app_version = xml.xpath(f'application[@name="{frontik_app.app_module_name}"]/version')[0]
 
         assert xml.tag == 'versions'
-        assert test_app_version.text == 'unknown'
+        assert test_app_version.text == '0.1.0'
 
     async def test_status(self) -> None:
         response = await self.fetch('/status')
@@ -25,3 +30,4 @@ class TestDefaultUrls(FrontikTestBase):
 
         json_response = json.loads(response.raw_body)
         assert 'uptime' in json_response
+        assert json_response['app_version'] == '0.1.0'
