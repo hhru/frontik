74b61e8143cefac50be6bf2af7b533c67ae2a231
Iskender Mamedov <i.mamedov@hh.ru>
2025-06-03 18:47:03 +0700

    HH-262842 fixed OpenAPI script by linters and added `types-pyyaml` dependency

--- frontik/util/generate_openapi.py
+++ frontik/util/generate_openapi.py
@@ -1,23 +1,24 @@
 import argparse
-import os
 import pathlib
 import sys
 
 import yaml
 from fastapi.openapi.utils import get_openapi
+
 from frontik.routing import import_all_pages, routers
 
-def main():
+
+def main() -> None:
     parser = argparse.ArgumentParser('generate OpenAPI spec from routes')
     parser.add_argument('--app_module_path', type=str, required=True)
     parser.add_argument('--version', type=str, required=True)
     args = parser.parse_args()
 
     module_path = pathlib.Path(args.app_module_path)
-    docs_path = os.path.join(module_path.parent, 'docs')
+    docs_path = module_path.parent / 'docs'
     sys.path.append(str(module_path.parent))
 
     import_all_pages(module_path.name)
     routes = [r for rout in routers for r in rout.routes]
     openapi = get_openapi(title=f'{module_path.name} OpenAPI', version=args.version, routes=routes)
-    pathlib.Path(os.path.join(os.path.abspath(docs_path)), 'openapi.yaml').write_text(yaml.dump(openapi))
+    pathlib.Path(docs_path.resolve(), 'openapi.yaml').write_text(yaml.dump(openapi))  # type: ignore[no-untyped-call]
